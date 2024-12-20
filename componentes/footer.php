0017a494e2a72f4fc064e2fc8e4f98d80934df26
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2024-12-20 17:03:15 -0300

    feat: footer

---
+++ componentes/footer.php
@@ -0,0 +1,5 @@
+<div class="bg-center bg-cover mx-auto flex flex-col justify-center items-center h-screen" style="background-image: url('/img/background_contacts.webp');">
+    <p class="font-inconsolata subtitle mt-20 text-purple-800">Contato</p>
+    <h1 class="text-white mt-4 text-bold font-asap title-md">Gostou do meu trabalho? </h1>
+    <p class="text-white font-maven-pro text-md mt-4 w-1/2 text-center">Entre em contato ou acompanhe as minhas redes sociais!</p>
+</div>
