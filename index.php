aadc1eb25483f73d92cc89ecc14a3f3619d4679e
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2024-12-20 11:22:16 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Desafio Prático - Portfólio Dev [PHP]</title>
+    <link href="https://fonts.googleapis.com/css2?family=Asap:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <?php
+    $tecnologias = [
+        "GitHub",
+        "PHP",
+        "CSS",
+        "HTML",
+        "Javascript"
+    ];
+    ?>
+
+    <?php include("componentes/header.php"); ?>
+    <section>
+    </section>
+    <footer>
+    </footer>
+</body>
+
+</html>
